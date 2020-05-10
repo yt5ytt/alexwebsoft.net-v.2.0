06ebe240125aebf69d47196344d27d78c517edb9
Alex Safranec <yt5ytt@gmail.com>
2020-05-10 22:13:12 +0200

    header update

--- header.php
+++ header.php
@@ -6,10 +6,9 @@
     <link rel="stylesheet" href="css/master.css">
     <link rel="stylesheet" href="css/animation.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
-    <title>AlexWEBSoft.net - <?php include("title.php"); ?></title>
-
 		<meta name="keywords" content="html, php, css, developer, web, designer, mysql, javascript, responsive, jquery, html5, css3" />
 		<meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE" />
 		<meta name="author" content="Aleksandar Šafranec, yt5ytt@gmail.com, +381605991001" />
 		<link rel="icon" type="img/png" href="img/favicon.png">
+    <title>AlexWEBSoft.net - <?php include("title.php"); ?></title>
   </head>
